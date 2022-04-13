3b7746c89b37521ea83b01c003f96f709e3ff656
snusun <0515hwang@snu.ac.kr>
2022-04-13 17:41:15 +0900

    HW2: apply buffered writer

--- HW2/code/ConsoleCommand.java
+++ HW2/code/ConsoleCommand.java
@@ -1,3 +1,5 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 
 /******************************************************************************
@@ -73,7 +75,8 @@ class DeleteCmd extends AbstractConsoleCommand {
 
 	@Override
 	public void apply(MovieDB db) throws Exception {
-		System.err.printf("[trace] DELETE [genre=%s, title=%s]\n", genre, movie);
+		//
+		//System.err.printf("[trace] DELETE [genre=%s, title=%s]\n", genre, movie);
 		
 		// 아래의 구현은 수정하지 않는 것이 바람직하다. MovieDB 의 내부 자료구조를
 		// 조작하는 코드는 MovieDB 안쪽에 존재하는 것이 바람직하다. 
@@ -102,7 +105,7 @@ class InsertCmd extends AbstractConsoleCommand {
 
 	@Override
 	public void apply(MovieDB db) throws Exception {
-		System.err.printf("[trace] INSERT [genre=%s, title=%s]\n", genre, movie);
+		//System.err.printf("[trace] INSERT [genre=%s, title=%s]\n", genre, movie);
 		db.insert(new MovieDBItem(genre, movie));
 	}
 }
@@ -120,16 +123,19 @@ class PrintCmd extends AbstractConsoleCommand {
 
 	@Override
 	public void apply(MovieDB db) throws Exception {
-		System.err.printf("[trace] PRINT\n");
+		//System.err.printf("[trace] PRINT\n");
 
 		MyLinkedList<MovieDBItem> result = db.items();
 
 		if (result.size() == 0) {
 			System.out.println("EMPTY");
 		} else {
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			for (MovieDBItem item : result) {
-				System.out.printf("(%s, %s)\n", item.getGenre(), item.getTitle());
+				bw.write("(" + item.getGenre() + ", " + item.getTitle() + ")\n");
+				//System.out.printf("(%s, %s)\n", item.getGenre(), item.getTitle());
 			}
+			bw.flush();
 		}
 	}
 }
@@ -150,7 +156,7 @@ class SearchCmd extends AbstractConsoleCommand {
 
 	@Override
 	public void apply(MovieDB db) throws Exception {
-		System.err.printf("[trace] SEARCH [%s]\n", term);
+		//System.err.printf("[trace] SEARCH [%s]\n", term);
 
 		MyLinkedList<MovieDBItem> result = db.search(term);
 
