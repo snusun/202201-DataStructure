302650ad74c97d37ec33c1d64dc3fecab0f689f1
snusun <0515hwang@snu.ac.kr>
2022-05-27 16:59:55 +0900

    HW5: implement search method

--- HW5/code/Matching.java
+++ HW5/code/Matching.java
@@ -2,131 +2,181 @@ import java.io.*;
 import java.util.LinkedList;
 import java.util.Hashtable;
 
-public class Matching
-{
-	static String[]	fileData;
-	static Hashtable<Integer, AVLTree<String, Position>> hashtable;
+public class Matching {
+    static String[] fileData;
+    static Hashtable<Integer, AVLTree<String, Position>> hashtable;
 
-	public static void main(String[] args)
-	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		while (true)
-		{
-			try
-			{
-				String input = br.readLine();
-				if (input.compareTo("QUIT") == 0)
-					break;
+        while (true) {
+            try {
+                String input = br.readLine();
+                if (input.compareTo("QUIT") == 0)
+                    break;
 
-				command(input);
+                command(input);
+            } catch (IOException e) {
+                System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
+            }
+        }
+    }
+
+    private static void command(String input) {
+        // TODO : 아래 문장을 삭제하고 구현해라.
+        //System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
+        String[] inputInfo = input.split(" ");
+        if (inputInfo[0].equals("<")) {
+            fileData = readFile(inputInfo[1]);
+            hashtable = makeHashTable(fileData);
+        } else if (inputInfo[0].equals("@")) {
+            int indexNumber = Integer.parseInt(inputInfo[1]);
+            findSlot(indexNumber);
+        } else if (inputInfo[0].equals("?")) {
+            String pattern = input.substring(2);
+            System.out.println(searchPattern(pattern));
+        }
+    }
+
+    private static String[] readFile(String filePath) {
+
+        LinkedList<String> strList = new LinkedList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+            String str;
+            while ((str = reader.readLine()) != null) {
+                strList.add(str);
+            }
+            reader.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String[] result = new String[strList.size()];
+        for (int i = 0; i < strList.size(); i++) {
+            result[i] = strList.get(i);
+        }
+
+        return result;
+    }
+
+    private static Hashtable<Integer, AVLTree<String, Position>> makeHashTable(String[] data) {
+        Hashtable<Integer, AVLTree<String, Position>> hashtable = new Hashtable<>();
+
+        for (int i = 0; i < data.length; i++) {
+            String str = data[i];
+            for (int j = 0; j < str.length() - 5; j++) {
+                String subString = str.substring(j, j + 6);
+                int slotNum = calculateSlotNum(subString);
+
+                if (hashtable.get(slotNum) == null) {
+                    hashtable.put(slotNum, new AVLTree<>());
+                }
+                hashtable.get(slotNum).insert(subString, new Position(i + 1, j + 1));
+            }
+        }
+
+        return hashtable;
+    }
+
+    private static void findSlot(int index) {
+        if (hashtable.get(index) == null) System.out.println("EMPTY");
+        else {
+            AVLTree<String, Position> avlTree = hashtable.get(index);
+            avlTree.preOrderTraversal(avlTree.getRoot());
+        }
+    }
+
+    private static String searchPattern(String pattern) {
+        String searchResult = "";
+
+        if (pattern.length() == 6) {
+            int slotNum = calculateSlotNum(pattern);
+
+            LinkedList<Position> positions = getPositions(slotNum, pattern);
+            if (positions == null) {
+                return "(0, 0)";
+            } else {
+                for (Position position : positions) {
+                    searchResult += position + " ";
+                }
+            }
+        } else { // 길이 6 초과
+            int slotNum = calculateSlotNum(pattern.substring(0, 6));
+
+            LinkedList<Position> positions = getPositions(slotNum, pattern.substring(0, 6));
+            if (positions == null) {
+                return "(0, 0)";
+            }
+            LinkedList<Position> concatPosition = new LinkedList<>();
+
+            for (Position start : positions) {
+                for (int i = 0; i < pattern.length(); i += 6) {
+                    if (pattern.length() - i <= 6) {
+                        int index = pattern.length() - 6;
+                        String lastStr = pattern.substring(index, pattern.length());
+                        int newSlotNum = calculateSlotNum(lastStr);
+                        LinkedList<Position> newPositions = getPositions(newSlotNum, lastStr);
+						if (newPositions==null) {
+							continue;
+						}
+						for (Position nextPosition : newPositions) {
+							if (start.j + index == nextPosition.j) {
+								if(!concatPosition.contains(start)) {
+									concatPosition.add(start);
+									concatPosition.add(start);
+								}
+							}
+						}
+
+                    } else {
+                        String subStr = pattern.substring(i, i + 6);
+                        int newSlotNum = calculateSlotNum(subStr);
+						LinkedList<Position> newPositions = getPositions(newSlotNum, subStr);
+                        if (newPositions==null) {
+                            continue;
+                        }
+
+                        boolean isExist = false;
+                        for (Position nextPosition : newPositions) {
+                            if (start.j + i == nextPosition.j) {
+                                isExist = true;
+                                break;
+                            }
+                        }
+                        if (!isExist) {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            for(Position position: concatPosition){
+            	searchResult += position + " ";
 			}
-			catch (IOException e)
-			{
-				System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
-			}
-		}
-	}
+        }
 
-	private static void command(String input)
-	{
-		// TODO : 아래 문장을 삭제하고 구현해라.
-		//System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
-		String[] inputInfo = input.split(" ");
-		if(inputInfo[0].equals("<")) {
-			fileData = readFile(inputInfo[1]);
-			hashtable = makeHashTable(fileData);
-		} else if(inputInfo[0].equals("@")) {
-			int indexNumber = Integer.parseInt(inputInfo[1]);
-			findSlot(indexNumber);
-		} else if(inputInfo[0].equals("?")) {
-			String pattern = input.substring(2);
-			String searchResult = searchPattern(pattern);
-			// print
-		}
-	}
+        return searchResult.trim();
+    }
 
-	private static String[] readFile(String filePath){
+    private static int calculateSlotNum(String str) {
+        char[] chars = str.toCharArray();
+        int ascii = 0;
+        for (char c : chars) {
+            ascii += c;
+        }
+        return ascii % 100;
+    }
 
-		LinkedList<String> strList = new LinkedList<>();
-
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(filePath));
-
-			String str;
-			while ((str = reader.readLine()) != null) {
-				strList.add(str);
-			}
-			reader.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		String[] result = new String[strList.size()];
-		for(int i=0; i<strList.size(); i++){
-			result[i] = strList.get(i);
-		}
-
-		//fileData = result;
-		return result;
-	}
-
-	private static Hashtable<Integer, AVLTree<String, Position>> makeHashTable(String[] data){
-		Hashtable<Integer, AVLTree<String, Position>> hashtable = new Hashtable<>();
-
-		//for(String str: data){
-		for(int i=0; i<data.length; i++){
-			String str = data[i];
-			for(int j=0; j<str.length()-5; j++){
-				String subString = str.substring(j, j+6);
-				char[] chars = subString.toCharArray();
-				int ascii = 0;
-				for(char c: chars){
-					ascii += c;
-				}
-				int slotNum = ascii % 100;
-				// 해당 key의 tree에 추가
-				if(hashtable.get(slotNum)==null){
-					hashtable.put(slotNum, new AVLTree<>());
-				}
-				hashtable.get(slotNum).insert(subString, new Position(i+1, j+1));
-			}
-		}
-
-		return hashtable;
-	}
-
-	private static void findSlot(int index){
-		if(hashtable.get(index)==null) System.out.println("EMPTY");
-		else {
-			AVLTree<String, Position> avlTree = hashtable.get(index);
-			avlTree.preOrderTraversal(avlTree.getRoot());
-		}
-		//return new LinkedList<>();
-	}
-
-	private static String searchPattern(String pattern){
-		// pattern 6글자씩 끊어서 검색
-		// 맨 처음 위치 검색 한 다음 위치 저장(여러개일 수 있으니 for loop)
-		// 각 위치마자 pattern 쪼갠 것 위치 찾아가며 이어지면 위치 list에 저장
-		// 마지막 부분이 6으로 떨어지지 않으면 뒤에서 부터 6글자 해서 검색 후 index 계산하기
-		char[] chars = pattern.toCharArray();
-		int ascii = 0;
-		for(char c: chars){
-			System.out.println(c + " " + (int)c);
-			ascii += c;
-		}
-		int slotNum = ascii % 100;
-		System.out.println(slotNum);
-
-		AVLTree<String, Position> avlTree = hashtable.get(slotNum);
-		AVLNode<String, Position> avlNode = avlTree.search(pattern);
-		System.out.println(avlNode.key);
-		for(Position position: avlNode.item){
-			System.out.println(position);
-		}
-
-		return "";
-	}
+    private static LinkedList<Position> getPositions(int slotNum, String pattern) {
+        if (hashtable.get(slotNum) == null) {
+            return null;
+        }
+        AVLTree<String, Position> avlTree = hashtable.get(slotNum);
+        AVLNode<String, Position> avlNode = avlTree.search(pattern);
+        return avlNode.item;
+    }
 }
